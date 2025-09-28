135b8eefa96df9c3a0a2570b35e88a9eb473f1f2
Ederson <jvedrson@gmail.com>
2025-09-28 01:44:23 -0400

    w3 weather activity

---
+++ scripts/weather.js
@@ -0,0 +1,35 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const WEATHER_API_KEY = '1a488ef871bb2b2692e50f9db8870afe';
+
+
+// OpenWeatherMap API URL for Trier, Germany
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=49.749992&lon=6.637143&units=metric&appid=${WEATHER_API_KEY}`;
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data); // Output results for testing
+            displayResults(data);
+        } else {
+            throw new Error(await response.text());
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function displayResults(data) {
+    currentTemp.textContent = data.main.temp;
+    const iconCode = data.weather[0].icon;
+    const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+    weatherIcon.setAttribute('src', iconUrl);
+    weatherIcon.setAttribute('alt', data.weather[0].description);
+    captionDesc.textContent = data.weather[0].description;
+}
+
+apiFetch();
